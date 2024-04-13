8d91d2dbccb620c49c5eaf3161efcd249512547a
Syeed Labeeb <137295297+s-labeeb@users.noreply.github.com>
2024-04-13 22:18:14 +0530

    Zomato Restaurant Analysis.sql

--- Zomato Restaurant Analysis.sql
+++ Zomato Restaurant Analysis.sql
@@ -1,11 +1,49 @@
-/*
- Q1. Build a Data Model using the Sheets in the Excel File
- */
-CREATE DATABASE ZOMATO;
-use ZOMATO;
-select*from zomata;
-select*from zomata_sql ;
-select*from zomata_sqll ;
+-- SHOW VARIABLES LIKE "secure_file_priv";
+/* USD_Rate Tabel imported using TABLE DATA IMPORT WIZARD */
+SELECT * FROM usd_rate;
+
+/* IMPORTING COUNTRY TABLE from Excel */
+
+create table Country
+(
+CountryId int,
+CountryName varchar(50)
+);
+
+Load data infile 'Zomato_Country.csv' into table country
+fields terminated by ',' /*.csv file*/
+Ignore 1 lines; 	/*First line of Excel file Contains Headers */
+
+select * from country;
+
+/* IMPORTING MAIN TABLE (Restaurant info.) from Excel*/
+
+create table Zomato 
+(
+RestaurantID bigint,
+restaurantName varchar(100),
+CountryCode int,
+city varchar(50),
+Locality varchar(50),
+Cuisines varchar(100),
+Currency varchar(50),
+Has_table_booking varchar (50),
+Has_online_delivery varchar(50),
+Is_delivering_now varchar(50),
+Price_range int,
+Votes int,
+Average_Cost_for_two int,
+Rating double,
+Datekey date
+);
+select count(*) from zomato;
+
+LOAD DATA INFILE 'Zomato_.csv' Into Table Zomato
+fields terminated by ','
+Ignore 1 lines;
+
+#Q Total number of Restaurants
+select count(*) from zomato;
 
 /*Q2. Build a Calendar Table using the Columns Datekey_Opening ( Which has Dates from Minimum Dates and Maximum Dates)
   Add all the below Columns in the Calendar Table using the Formulas.
@@ -19,35 +57,24 @@ select*from zomata_sqll ;
    H.FinancialMOnth ( April = FM1, May= FM2  …. March = FM12)
    I. Financial Quarter ( Quarters based on Financial Month FQ-1 . FQ-2..)
    */
-CREATE TABLE Calendar (
-    DateKey DATE ,
-    Year INT,
-    MonthNo INT,
-    MonthFullName VARCHAR(20),
-    Quarter VARCHAR(5),
-    YearMonth VARCHAR(8),
-    WeekdayNo INT,
-    WeekdayName VARCHAR(20),
-    FinancialMonth INT,
-    FinancialQuarter INT
-);
-INSERT INTO Calendar (DateKey, Year, MonthNo, MonthFullName, Quarter, YearMonth, WeekdayNo, WeekdayName, FinancialMonth, FinancialQuarter)
+
+create view Date_Table as
 SELECT
     Datekey_Opening AS DateKey,
     YEAR(Datekey_Opening) AS Year,
     MONTH(Datekey_Opening) AS MonthNo,
-    MONTHNAME(Datekey_Opening) AS MonthFullName,
+    MONTHNAME(Datekey_Opening) AS Month_Name,
     CONCAT('Q', QUARTER(Datekey_Opening)) AS Quarter,
-    CONCAT(YEAR(Datekey_Opening), '-', LPAD(MONTH(Datekey_Opening), 2, '0')) AS YearMonth,
+   date_format(datekey, '%Y-%b') as YearMonth,
     DAYOFWEEK(Datekey_Opening) AS WeekdayNo,
     DAYNAME(Datekey_Opening) AS WeekdayName,
     IF(MONTH(Datekey_Opening) < 4, MONTH(Datekey_Opening) + 8, MONTH(Datekey_Opening) - 3) AS FinancialMonth,
     IF(MONTH(Datekey_Opening) < 4, QUARTER(Datekey_Opening) + 3, QUARTER(Datekey_Opening) - 1) AS FinancialQuarter
-FROM zomata;
-select*from Calendar;
+FROM zomato;
+
 
 /*
-Q3. Convert the Average cost for 2 column into USD dollars (currently the Average cost for 2 in local currencies
+Q3. Convert the Average cost for 2 columns into USD dollars (currently the Average cost for 2 in local currencies
 */
 SELECT
     z.Average_Cost_for_two,
@@ -59,7 +86,7 @@ JOIN
     zomata_sqll e ON z.Currency = e.Currency;
 
 /*
-Q4.Find the Numbers of Resturants based on City and Country.
+Q4.Find the Numbers of Restaurants based on City and Country.
 */
 SELECT
     z.City,
@@ -74,7 +101,7 @@ GROUP BY
     s.countryname;
 
 /*
-Q5.Numbers of Resturants opening based on Year , Quarter , Month
+Q5.Numbers of restaurants opening based on Year, Quarter, Month
 */
 SELECT
     YEAR(c.DateKey) AS Year,
@@ -91,18 +118,18 @@ GROUP BY
     MONTH(c.DateKey);
 
 /*
-Q6. Count of Resturants based on Average Ratings
+Q6. Count of Restaurants based on Average Ratings
 */
 SELECT
     ROUND(AVG(Rating), 1) AS AvgRating,
     COUNT(*) AS NumRestaurants
 FROM
-    zomata
+    zomato
 GROUP BY
     ROUND(Rating, 1);
 
 /*
-Q7. Create buckets based on Average Price of reasonable size and find out how many resturants falls in each buckets
+Q7. Create buckets based on Average Price of reasonable size and find out how many restaurants fall in each bucket
 */
 SELECT
     CASE
@@ -113,31 +140,31 @@ SELECT
     END AS PriceBucket,
     COUNT(*) AS NumRestaurants
 FROM
-    zomata
+    zomato
 GROUP BY
     PriceBucket;
 
 /*
-Q8.Percentage of Resturants based on "Has_Table_booking"
+Q8.Percentage of Restaurants based on "Has_Table_booking"
 */
 SELECT
     Has_Table_booking,
     COUNT(*) AS NumRestaurants,
-    (COUNT(*) / (SELECT COUNT(*) FROM zomata)) * 100 AS Percentage
+    (COUNT(*) / (SELECT COUNT(*) FROM zomato)) * 100 AS Percentage
 FROM
-    zomata
+    zomato
 GROUP BY
     Has_Table_booking;
 
 /*
-Q9.Percentage of Resturants based on "Has_Online_delivery"
+Q9.Percentage of Restaurants based on "Has_Online_delivery"
 */
 SELECT
     Has_Online_delivery,
     COUNT(*) AS NumRestaurants,
-    (COUNT(*) / (SELECT COUNT(*) FROM zomata)) * 100 AS Percentage
+    (COUNT(*) / (SELECT COUNT(*) FROM zomato)) * 100 AS Percentage
 FROM
-    zomata
+    zomato
 GROUP BY
     Has_Online_delivery;
 
@@ -146,19 +173,19 @@ GROUP BY
 */
 -- 1>Cuisines Analysis:
 SELECT Cuisines, COUNT(*) AS NumRestaurants
-FROM zomata
+FROM zomato
 GROUP BY Cuisines
 ORDER BY NumRestaurants DESC
 LIMIT 10;
 
 -- 2> City Analysis
 SELECT City, COUNT(*) AS NumRestaurants
-FROM zomata
+FROM zomato
 GROUP BY City
 ORDER BY NumRestaurants DESC
 LIMIT 10;
 
 -- 3> Rating Analysis
 SELECT Rating, COUNT(*) AS NumRestaurants
-FROM zomata
+FROM zomato
 GROUP BY Rating;
